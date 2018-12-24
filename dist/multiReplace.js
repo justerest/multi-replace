f88d66ae42dbdd4c95ba51384bb5b94119a0aabe
Sergey Klevakin <justerest@yandex.ru>
2018-12-24 16:39:20 +0500

    fix: recursively replacement

--- dist/multiReplace.js
+++ dist/multiReplace.js
@@ -2,12 +2,24 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const lodash_1 = require("lodash");
 const pascalCase_1 = require("./utils/pascalCase");
+const uniquePlaceholder = '__unique-multiReplace-placeholder__';
 function multiReplace(str, searchValue, replaceValue) {
-    return str
-        .replace(new RegExp(lodash_1.kebabCase(searchValue), 'g'), lodash_1.kebabCase(replaceValue))
-        .replace(new RegExp(lodash_1.snakeCase(searchValue).toUpperCase(), 'g'), lodash_1.snakeCase(replaceValue).toUpperCase())
-        .replace(new RegExp(lodash_1.snakeCase(searchValue), 'g'), lodash_1.snakeCase(replaceValue))
-        .replace(new RegExp(pascalCase_1.pascalCase(searchValue), 'g'), pascalCase_1.pascalCase(replaceValue))
-        .replace(new RegExp(lodash_1.camelCase(searchValue), 'g'), lodash_1.camelCase(replaceValue));
+    return multiDeserialize(multiSerialize(str, searchValue), replaceValue);
 }
 exports.multiReplace = multiReplace;
+function multiSerialize(str, searchValue) {
+    return str
+        .replace(new RegExp(lodash_1.camelCase(searchValue), 'g'), lodash_1.camelCase(uniquePlaceholder))
+        .replace(new RegExp(lodash_1.kebabCase(searchValue), 'g'), lodash_1.kebabCase(uniquePlaceholder))
+        .replace(new RegExp(pascalCase_1.pascalCase(searchValue), 'g'), pascalCase_1.pascalCase(uniquePlaceholder))
+        .replace(new RegExp(lodash_1.snakeCase(searchValue), 'g'), lodash_1.snakeCase(uniquePlaceholder))
+        .replace(new RegExp(lodash_1.snakeCase(searchValue).toUpperCase(), 'g'), lodash_1.snakeCase(uniquePlaceholder).toUpperCase());
+}
+function multiDeserialize(str, replaceValue) {
+    return str
+        .replace(new RegExp(lodash_1.camelCase(uniquePlaceholder), 'g'), lodash_1.camelCase(replaceValue))
+        .replace(new RegExp(lodash_1.kebabCase(uniquePlaceholder), 'g'), lodash_1.kebabCase(replaceValue))
+        .replace(new RegExp(pascalCase_1.pascalCase(uniquePlaceholder), 'g'), pascalCase_1.pascalCase(replaceValue))
+        .replace(new RegExp(lodash_1.snakeCase(uniquePlaceholder), 'g'), lodash_1.snakeCase(replaceValue))
+        .replace(new RegExp(lodash_1.snakeCase(uniquePlaceholder).toUpperCase(), 'g'), lodash_1.snakeCase(replaceValue).toUpperCase());
+}
--- src/multiReplace.ts
+++ src/multiReplace.ts
@@ -1,11 +1,26 @@
 import { camelCase, kebabCase, snakeCase } from 'lodash';
 import { pascalCase } from './utils/pascalCase';
 
+const uniquePlaceholder = '__unique-multiReplace-placeholder__';
+
 export function multiReplace(str: string, searchValue: string, replaceValue: string): string {
+    return multiDeserialize(multiSerialize(str, searchValue), replaceValue);
+}
+
+function multiSerialize(str: string, searchValue: string) {
     return str
-        .replace(new RegExp(kebabCase(searchValue), 'g'), kebabCase(replaceValue))
-        .replace(new RegExp(snakeCase(searchValue).toUpperCase(), 'g'), snakeCase(replaceValue).toUpperCase())
-        .replace(new RegExp(snakeCase(searchValue), 'g'), snakeCase(replaceValue))
-        .replace(new RegExp(pascalCase(searchValue), 'g'), pascalCase(replaceValue))
-        .replace(new RegExp(camelCase(searchValue), 'g'), camelCase(replaceValue));
+        .replace(new RegExp(camelCase(searchValue), 'g'), camelCase(uniquePlaceholder))
+        .replace(new RegExp(kebabCase(searchValue), 'g'), kebabCase(uniquePlaceholder))
+        .replace(new RegExp(pascalCase(searchValue), 'g'), pascalCase(uniquePlaceholder))
+        .replace(new RegExp(snakeCase(searchValue), 'g'), snakeCase(uniquePlaceholder))
+        .replace(new RegExp(snakeCase(searchValue).toUpperCase(), 'g'), snakeCase(uniquePlaceholder).toUpperCase());
+}
+
+function multiDeserialize(str: string, replaceValue: string): string {
+    return str
+        .replace(new RegExp(camelCase(uniquePlaceholder), 'g'), camelCase(replaceValue))
+        .replace(new RegExp(kebabCase(uniquePlaceholder), 'g'), kebabCase(replaceValue))
+        .replace(new RegExp(pascalCase(uniquePlaceholder), 'g'), pascalCase(replaceValue))
+        .replace(new RegExp(snakeCase(uniquePlaceholder), 'g'), snakeCase(replaceValue))
+        .replace(new RegExp(snakeCase(uniquePlaceholder).toUpperCase(), 'g'), snakeCase(replaceValue).toUpperCase());
 }
