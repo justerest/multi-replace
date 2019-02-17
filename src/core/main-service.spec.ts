1dd9fbb2a54bd61ed9dbcdcaa414b02a4c82097c
justerest <justerest@yandex.com>
2019-02-17 23:17:56 +0500

    refactor: rename main service

--- src/core/main-service.spec.ts
+++ src/core/main-service.spec.ts
@@ -1,28 +1,28 @@
 import { of } from 'rxjs';
 
 import { FileSystemService } from './file-system-service';
-import { MultiReplacer } from './multi-replacer';
+import { MainService } from './main-service';
 
-describe('MultiReplacer', () => {
-	let multiReplacer: MultiReplacer;
+describe('MainService', () => {
+	let mainService: MainService;
 	let mockFileService: FileSystemService;
 
 	beforeEach(() => {
 		mockFileService = new FileSystemService();
 		mockFileService.writeFile = async () => void 0;
 		mockFileService.moveFile = async () => void 0;
-		multiReplacer = new MultiReplacer(void 0, mockFileService);
+		mainService = new MainService(void 0, mockFileService);
 	});
 
 	it('should be created', () => {
-		expect(multiReplacer).toBeTruthy();
+		expect(mainService).toBeTruthy();
 	});
 
 	describe('multiReplace method', () => {
 		it('should replace preserving cases', (complete) => {
 			expect.assertions(1);
-			multiReplacer.getFilesData = () => of({ basePath: 'core', srcPath: 'core/asd.ts', srcText: 'testText' });
-			multiReplacer.multiReplace([], 'testText', 'text-test')
+			mainService.getFilesData = () => of({ basePath: 'core', srcPath: 'core/asd.ts', srcText: 'testText' });
+			mainService.multiReplace([], 'testText', 'text-test')
 				.subscribe((data) => expect(data.outText).toBe('textTest'), void 0, complete);
 		});
 	});
