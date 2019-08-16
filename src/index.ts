c47d66661c98be053615dc6955663ed22e155550
justerest <justerest@gmailcom>
2019-08-16 21:39:48 +0500

    feat: multi replace including selected folder function

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,28 @@
 import { CopyFilesParserImp } from './copy-files-parser-imp';
+import { DirFilePathTransformerImp } from './dir-file-path-transformer-imp';
 import { MultiReplaceService } from './multi-replace-service';
 import { StringTransformerImp } from './string-transformer-imp';
+
+export * from './models/file-path-transformer';
+export * from './models/file-system-service';
+export * from './models/files-parser';
+export * from './models/multi-replace-params';
+export * from './models/string-transformer';
 
 export * from './file-system-service-imp';
 export * from './string-transformer-imp';
 export * from './multi-replace-service';
 export * from './dir-file-path-transformer-imp';
 export * from './strict-file-path-transformer-imp';
+export * from './files-parser-imp';
+export * from './copy-files-parser-imp';
 
 const multiReplaceService = new MultiReplaceService();
-const multiReplaceServiceStrict = new MultiReplaceService(new StringTransformerImp([(str) => str]));
 const multiReplaceCopyService = new MultiReplaceService(void 0, void 0, void 0, new CopyFilesParserImp());
+const multiReplaceWithFolderService = new MultiReplaceService(void 0, void 0, new DirFilePathTransformerImp());
+const multiReplaceServiceStrict = new MultiReplaceService(new StringTransformerImp([(str) => str]));
 
 export const multiReplace = multiReplaceService.multiReplace.bind(multiReplaceService);
-export const multiReplaceStrict = multiReplaceServiceStrict.multiReplace.bind(multiReplaceServiceStrict);
 export const multiReplaceCopy = multiReplaceCopyService.multiReplace.bind(multiReplaceCopyService);
+export const multiReplaceWithFolder = multiReplaceWithFolderService.multiReplace.bind(multiReplaceWithFolderService);
+export const multiReplaceStrict = multiReplaceServiceStrict.multiReplace.bind(multiReplaceServiceStrict);
